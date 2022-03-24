8618fa7eb9ed1af5222be65b7db87035d7335e38
Bohdan <brusnachenko@gmail.com>
2022-03-24 21:11:10 +0200

    SMT1

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,65 @@
 <template>
-
+  <div class="app">
+    <post-form
+      @create="createPost"
+    />
+    <post-list
+      :posts="posts"
+    />
+  </div>
 </template>
 
 <script>
+import PostForm from '@/components/PostForm'
+import PostList from '@/components/PostList'
 export default {
-  name: "App"
+  components: {
+    PostForm,
+    PostList
+  },
+
+  data () {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: 'JS 1',
+          body: 'Description 1'
+        },
+        {
+          id: 2,
+          title: 'JS 2',
+          body: 'Description 2'
+        },
+        {
+          id: 3,
+          title: 'JS 3',
+          body: 'Description 3'
+        },
+        {
+          id: 4,
+          title: 'JS 4',
+          body: 'Description 4'
+        }
+      ]
+    }
+  },
+  methods: {
+    createPost (post) {
+      this.posts.push(post)
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
 
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>
+      User list
+    </h3>
+    <post-item
+      class="post" v-for="(post, idx) in posts"
+      :key="idx"
+      :post="post"
+    />
+  </div>
+
+</template>
+
+<script>
+import PostItem from '@/components/PostItem'
+export default {
+  components: { PostItem },
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
